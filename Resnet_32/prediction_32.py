137f9fe8d5c9da7b8972f347b508588080a75606
whitewalker007 <nitinkrshukla@gmail.com>
2017-07-06 21:55:15 +0530

    Made few changes

--- Resnet_32/prediction_32.py
+++ Resnet_32/prediction_32.py
@@ -52,7 +52,7 @@ mean_array = np.asarray(mean_blob.data, dtype=np.float32).reshape(
 
 #Read model architecture and trained model's weights
 net = caffe.Net('/home/ubuntu/DeepLearning_crop_classification/Caffe_Model_Leaves/Resnet_32/deploy_32.prototxt',
-                '/home/ubuntu/DeepLearning_crop_classification/Caffe_Model_Leaves/Resnet_32/Resnet_Leaves_32_iter_10000.caffemodel',
+                '/home/ubuntu/DeepLearning_crop_classification/Caffe_Model_Leaves/Resnet_32/Resnet_32_iter_5000.caffemodel',
                 caffe.TEST)
 
 #Define image transformers
@@ -67,28 +67,23 @@ Making predicitions
 test_img_paths = [img_path for img_path in glob.glob("/home/ubuntu/DeepLearning_crop_classification/input/LEAVES/test/*jpg")]
 
 #Making predictions
-test_ids = []
-preds = []
-for img_path in test_img_paths:
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    img = transform_img(img, img_width=IMG_WIDTH, img_height=IMG_HEIGHT)
+with open("/home/ubuntu/DeepLearning_crop_classification/Caffe_Model_Leaves/Resnet_32/Resnet_Leaves_32.csv","w") as f:
+    f.write("Id,SUGARCANE,SOYBEAN,RICE,MAIZE,GROUNDNUT,LABEL\n")
+    for img_path in test_img_paths:
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = transform_img(img, img_width=IMG_WIDTH, img_height=IMG_HEIGHT)
 
-    net.blobs['data'].data[...] = transformer.preprocess('data', img)
-    out = net.forward()
-    pred_probas = out['prob']
+        net.blobs['data'].data[...] = transformer.preprocess('data', img)
+        out = net.forward()
+        pred_probas = out['prob']
 
-    test_ids = test_ids + [img_path.split('/')[-1][:-4]]
-    preds = preds + [pred_probas.argmax()]
+        test_ids = img_path.split('/')[-1][:-4]
+        preds = pred_probas.argmax()
 
-    print img_path
-    print pred_probas.argmax()
-    print '-------'
+        print img_path
+        print pred_probas
+        print '-------'
 
-'''
-Making submission file
-'''
-with open("/home/ubuntu/DeepLearning_crop_classification/Caffe_Model_Leaves/Resnet_32/Resnet_leaves_32.csv","w") as f:
-    f.write("id,label\n")
-    for i in range(len(test_ids)):
-        f.write(str(test_ids[i])+","+str(preds[i])+"\n")
-f.close()
+
+        f.write(str(test_ids)+","+str(pred_probas[0,0])+","+str(pred_probas[0,1])+","+str(pred_probas[0,2])+","+str(pred_probas[0,3])+","+str(pred_probas[0,4])+","+str(preds)+"\n")
+    f.close()
